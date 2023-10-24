e4d55f8aec30b1c5c70c417fd6050098cd8648ec
Magi-3v8 <mourapastel@gmail.com>
2023-10-24 18:16:16 -0300

    Concertando connection do db

--- src/HortiDot/Config/DataContext.cs
+++ src/HortiDot/Config/DataContext.cs
@@ -8,5 +8,10 @@ namespace HortiDot.Config
         public DataContext(DbContextOptions<DataContext> options ) : base(options) { }
         
         public DbSet<Fornecedor> fornecedores { get; set; }
+
+        public DbSet<Comprador> compradores { get; set; }
+
+        public DbSet<Cotacao> cotacoes { get; set; }
+
     }
 }
