b8a9c7c82b45f7630a382a043fd1582a27a45710
Magi-3v8 <mourapastel@gmail.com>
2023-10-27 00:03:43 -0300

    adicionando arquivos de login

---
+++ src/HortiDot/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HortiDot.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
