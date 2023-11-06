9dda990c8e076ab7d20c084d2d822e4c6d1f4d91
Ingrid da Rosa Santana de Barros <ingrid.barros.1439833@sga.pucminas.br>
2023-11-06 19:57:01 -0300

    Adicionando Model Pedido

---
+++ src/HortiDot/Models/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HortiDot.Models
+{
+    [Table("Pedidos")]
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<Produto> ListaItens { get; set; }
+        public string DataPedido { get; set; }
+        public string StatusPedidos { get; set; }
+        public decimal ValorTotal { get; set; }
+        public int CodigoComprador { get; set; }
+        public int CodigoFornecedor { get; set; }
+    }
+}
