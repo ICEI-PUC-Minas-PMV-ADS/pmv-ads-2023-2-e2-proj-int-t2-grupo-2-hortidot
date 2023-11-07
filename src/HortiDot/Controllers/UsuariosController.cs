2a8f128d9b7607ddbaedf87c5531f6903cddc654
Karina Barbosa <kgbarbosa@sga.pucminas.br>
2023-11-07 17:51:04 -0300

    Alteração Usuarios Controller

--- src/HortiDot/Controllers/UsuariosController.cs
+++ src/HortiDot/Controllers/UsuariosController.cs
@@ -1,7 +1,12 @@
-﻿using HortiDot.Config;
-using HortiDot.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using HortiDot.Config;
+using HortiDot.Models;
 
 namespace HortiDot.Controllers
 {
@@ -14,25 +19,107 @@ namespace HortiDot.Controllers
             _context = context;
         }
 
-        public IActionResult CriarConta()
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.usuarios.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.usuarios
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public async Task<IActionResult> CriarConta([Bind("ID,Nome,Email,Cpf,Telefone,NomeEmpresa,CNPJ,Endereco,Senha,TipoDeUsuario,Contatos,Pedidos")] Usuario usuario)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nome,Email,Cpf,Telefone,NomeEmpresa,CNPJ,Endereco,Senha,Pedidos,Contatos,TipoDeUsuario")] Usuario usuario)
         {
-            usuario.Pedidos = "";
-            usuario.Contatos = "";
+            if (ModelState.IsValid)
+            {
 
-            usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
-
-            _context.Add(usuario);
-            await _context.SaveChangesAsync();
-            return Redirect("/");
-           
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
         }
 
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,Email,Cpf,Telefone,NomeEmpresa,CNPJ,Endereco,Senha,Pedidos,Contatos,TipoDeUsuario")] Usuario usuario)
+        {
+            if (id != usuario.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.usuarios == null)
@@ -40,33 +127,38 @@ namespace HortiDot.Controllers
                 return NotFound();
             }
 
-            var cotacao = await _context.cotacoes
+            var usuario = await _context.usuarios
                 .FirstOrDefaultAsync(m => m.ID == id);
-            if (cotacao == null)
+            if (usuario == null)
             {
                 return NotFound();
             }
 
-            return View(cotacao);
+            return View(usuario);
         }
 
-        // POST: Cotacaos/Delete/5
+        // POST: Usuarios/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.cotacoes == null)
+            if (_context.usuarios == null)
             {
-                return Problem("Entity set 'DataContext.cotacoes'  is null.");
+                return Problem("Entity set 'DataContext.usuarios'  is null.");
             }
-            var cotacao = await _context.cotacoes.FindAsync(id);
-            if (cotacao != null)
+            var usuario = await _context.usuarios.FindAsync(id);
+            if (usuario != null)
             {
-                _context.cotacoes.Remove(cotacao);
+                _context.usuarios.Remove(usuario);
             }
-
+            
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+          return _context.usuarios.Any(e => e.ID == id);
         }
     }
 }
