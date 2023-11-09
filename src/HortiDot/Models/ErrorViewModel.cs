3169dc1ad8fca1f75ae234d6c365d94eec59e220
Magi-3v8 <mourapastel@gmail.com>
2023-11-09 13:00:56 -0300

    Initial

--- src/HortiDot/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HortiDot.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
